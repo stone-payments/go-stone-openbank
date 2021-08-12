ad6a1b20143c5e946777d87f619bc0e62211d34d
Gabriel Costa <gabrielcerqueira96@hotmail.com>
2021-08-12 09:50:18 -0300

    refactor: go fmt institution struct

--- types/institution.go
+++ types/institution.go
@@ -1,8 +1,8 @@
 package types
 
 type Institution struct {
-	ISPBCode       string    `json:"ispb_code"`
-	NumberCode     string    `json:"number_code"`
+	ISPBCode       string `json:"ispb_code"`
+	NumberCode     string `json:"number_code"`
 	Name           string `json:"name"`
 	ShortName      string `json:"short_name"`
 	SPIParticipant bool   `json:"spi_participant"`
