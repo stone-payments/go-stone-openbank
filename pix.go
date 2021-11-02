6642fa8875eb4c91c63482e9980d85365327a02a
Guilherme Marcial <guilherme.marcial.felipe@gmail.com>
2021-11-01 22:33:48 -0400

    feature(pix): implementing the endpoint to list keys pix (#38)

--- pix.go
+++ pix.go
@@ -48,3 +48,25 @@ func (s *PIXService) GetQRCodeData(input types.GetQRCodeInput) (*types.QRCode, *
 
 	return &qrcode, resp, err
 }
+
+//ListKeys list the keys PIX of an account
+func (s *PIXService) ListKeys(accountID string, idempotencyKey string) ([]types.KeyPIX, *Response, error){
+	path := fmt.Sprintf("/api/v1/pix/%s/entries", accountID)
+
+	req, err := s.client.NewAPIRequest(http.MethodGet, path, idempotencyKey, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var dataResp struct {
+		Cursor types.Cursor `json:"cursor"`
+		Data   []types.KeyPIX  `json:"data"`
+	}
+
+	resp, err := s.client.Do(req, &dataResp)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return dataResp.Data, resp, err
+}
